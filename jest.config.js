a74a203fd584abe942f8f509404d665e02fb452c
tany <valehova.tany@gmail.com>
2021-03-04 01:24:45 +0300

    clean branch

--- jest.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    clearMocks: true,
-    coverageDirectory: "coverage",
-    testEnvironment: "jsdom",
-    setupFilesAfterEnv: ["<rootDir>/internals/jestSettings.js"],
-    transform: {
-        "^.+\\.(js|jsx|ts|tsx)$": "<rootDir>/node_modules/babel-jest",
-    },
-};
